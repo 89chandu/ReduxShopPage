427a0d03f2332cd5310729f83cdab95813cb1c02
Chandu Bopche <Chandubopche321@gmail.com>
2023-08-18 13:10:53 +0530

    Doing Network calls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import store from '../src/components/store/index';
+import store from './store/index';
 import './index.css';
 import App from './App';
 
